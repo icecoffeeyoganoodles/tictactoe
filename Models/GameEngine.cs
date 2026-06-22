a6714a0808959b70ffc2e48b65efba1940824c5a
icecoffeeyoganoodles <nicolasbaloghtictactoe@gmail.com>
2026-06-22 18:06:54 +0200

    preparing UI and fixing import

--- Models/GameEngine.cs
+++ Models/GameEngine.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
-using TicTacToe.Desktop.Enums; // ou .Models selon ton organisation
-
+using TicTacToe.Desktop.Enums;
 namespace TicTacToe.Desktop.Models;
 
 public class GameEngine
